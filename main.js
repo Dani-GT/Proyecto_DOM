8426e4b9a938de9a8ba43faacbe389ef3405cfcc
Dani-GT <dani_spain87@hotmail.com>
2024-09-08 17:50:59 +0200

    add images al array

--- main.js
+++ main.js
@@ -2,73 +2,67 @@ const products = [
     {
       nombre: "Pure Whey Isolate 95 - 2.2 kg",
       vendedor: "Olimp Sport Nutrition",
-      urlImagen: "https://www.pontemasfuerte.com/producto-proteina.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/1762/pure-whey-isolate-95-1473497167.jpg?1664789297",
       precio: 97.37
     },
     {
       nombre: "Sports Vitamins - 60 caps.",
       vendedor: "Perfect Nutrition",
-      urlImagen: "https://www.pontemasfuerte.com/producto-vitaminas.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/955/sports-vitamins-1722605582.jpeg?1722605582",
       precio: 13.95
-    },
-    {
-      nombre: "BCAA X Tank - 120 caps.",
-      vendedor: "Trec Nutrition",
-      urlImagen: "https://www.pontemasfuerte.com/producto-bcaa.jpg",
-      precio: 23.61
     },
     {
       nombre: "Iso Whey Zero - 2.27 kg",
       vendedor: "Biotech USA",
-      urlImagen: "https://www.pontemasfuerte.com/producto-whey.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/2439/iso-whey-zero-1620313289.jpg?1664794356",
       precio: 89.90
     },
     {
       nombre: "Glutamine Micronized - 500 gr",
       vendedor: "AMIX Performance",
-      urlImagen: "https://www.pontemasfuerte.com/producto-glutamina.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/515/glutamine-micropowder-500-gr-1482308743.jpg?1664788206",
       precio: 25.90
     },
     {
       nombre: "Creatina Creapure - 300 gr",
       vendedor: "Quamtrax Nutrition",
-      urlImagen: "https://www.pontemasfuerte.com/producto-creatina.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/10801/creatine-monohydrate-creapure-lollipop-1682329737.png",
       precio: 19.90
     },
     {
       nombre: "L-Carnitina 3000 - 20 ampollas",
-      vendedor: "226ERS",
-      urlImagen: "https://www.pontemasfuerte.com/producto-carnitina.jpg",
-      precio: 27.50
+      vendedor: "Quamtrax Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/1853/l-carnitine-3000-20-viales-1584359519.jpg?1664789305",
+      precio: 19.90
     },
     {
       nombre: "Omega 3 - 90 perlas",
       vendedor: "Now Foods",
-      urlImagen: "https://www.pontemasfuerte.com/producto-omega3.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/7321/omega-3-weider-1697809081.jpg?1697809081",
       precio: 18.99
     },
     {
       nombre: "Amino BCAA 2:1:1 - 300 tabls.",
       vendedor: "Trec Nutrition",
-      urlImagen: "https://www.pontemasfuerte.com/producto-amino.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/5830/super-bcaa-system-1624454468.jpg?1664802970",
       precio: 43.90
     },
     {
       nombre: "Proteína de guisante - 1 kg",
       vendedor: "Haya Labs",
-      urlImagen: "https://www.pontemasfuerte.com/producto-proteina-guisante.jpg",
+      urlImagen: "https://www.hayalabs.es/937-thickbox_default/proteina-vegana-750-grms-fresa-natural.jpg",
       precio: 22.50
     },
     {
       nombre: "Gainer Mass XXL - 4 kg",
       vendedor: "Max Protein",
-      urlImagen: "https://www.pontemasfuerte.com/producto-gainer.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/5828/mass-xxl-1624453423.jpg?1664802831",
       precio: 49.90
     },
     {
       nombre: "Casein Zero - 908 gr",
       vendedor: "Biotech USA",
-      urlImagen: "https://www.pontemasfuerte.com/producto-casein.jpg",
+      urlImagen: "https://www.pontemasfuerte.com/media/products/2449/casein-zero-1590408940.jpg?1664794358",
       precio: 38.90
     }
   ];
