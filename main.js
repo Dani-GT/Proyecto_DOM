85bce5cac54d984092ad2a1dcd106c6a90f50255
Dani-GT <dani_spain87@hotmail.com>
2024-09-08 17:37:45 +0200

    add Array

---
+++ main.js
@@ -0,0 +1,75 @@
+const products = [
+    {
+      nombre: "Pure Whey Isolate 95 - 2.2 kg",
+      vendedor: "Olimp Sport Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/producto-proteina.jpg",
+      precio: 97.37
+    },
+    {
+      nombre: "Sports Vitamins - 60 caps.",
+      vendedor: "Perfect Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/producto-vitaminas.jpg",
+      precio: 13.95
+    },
+    {
+      nombre: "BCAA X Tank - 120 caps.",
+      vendedor: "Trec Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/producto-bcaa.jpg",
+      precio: 23.61
+    },
+    {
+      nombre: "Iso Whey Zero - 2.27 kg",
+      vendedor: "Biotech USA",
+      urlImagen: "https://www.pontemasfuerte.com/producto-whey.jpg",
+      precio: 89.90
+    },
+    {
+      nombre: "Glutamine Micronized - 500 gr",
+      vendedor: "AMIX Performance",
+      urlImagen: "https://www.pontemasfuerte.com/producto-glutamina.jpg",
+      precio: 25.90
+    },
+    {
+      nombre: "Creatina Creapure - 300 gr",
+      vendedor: "Quamtrax Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/producto-creatina.jpg",
+      precio: 19.90
+    },
+    {
+      nombre: "L-Carnitina 3000 - 20 ampollas",
+      vendedor: "226ERS",
+      urlImagen: "https://www.pontemasfuerte.com/producto-carnitina.jpg",
+      precio: 27.50
+    },
+    {
+      nombre: "Omega 3 - 90 perlas",
+      vendedor: "Now Foods",
+      urlImagen: "https://www.pontemasfuerte.com/producto-omega3.jpg",
+      precio: 18.99
+    },
+    {
+      nombre: "Amino BCAA 2:1:1 - 300 tabls.",
+      vendedor: "Trec Nutrition",
+      urlImagen: "https://www.pontemasfuerte.com/producto-amino.jpg",
+      precio: 43.90
+    },
+    {
+      nombre: "Proteína de guisante - 1 kg",
+      vendedor: "Haya Labs",
+      urlImagen: "https://www.pontemasfuerte.com/producto-proteina-guisante.jpg",
+      precio: 22.50
+    },
+    {
+      nombre: "Gainer Mass XXL - 4 kg",
+      vendedor: "Max Protein",
+      urlImagen: "https://www.pontemasfuerte.com/producto-gainer.jpg",
+      precio: 49.90
+    },
+    {
+      nombre: "Casein Zero - 908 gr",
+      vendedor: "Biotech USA",
+      urlImagen: "https://www.pontemasfuerte.com/producto-casein.jpg",
+      precio: 38.90
+    }
+  ];
+  
